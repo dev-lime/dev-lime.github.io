51eda408ab4e60d490e28a9ef3b6bd2b16e6de4e
l1me <102348214+l1me0ffical@users.noreply.github.com>
2023-12-04 10:50:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function submitFeedback() {
   // Ваш код обработки данных формы, например, отправка на сервер
 
   // Заменить блок обратной связи на сообщение благодарности
-  document.getElementById('feedbackBlock').innerHTML = '<p>Спасибо за отзыв!</p>';
+  document.getElementById('feedback-card').innerHTML = '<p>Спасибо за отзыв!</p>';
 }
