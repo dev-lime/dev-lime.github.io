86f585f97826fa42e488a780ec121e281f708afc
l1me <102348214+l1me0ffical@users.noreply.github.com>
2023-12-04 10:47:33 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+function submitFeedback() {
+  // Ваш код обработки данных формы, например, отправка на сервер
+
+  // Заменить блок обратной связи на сообщение благодарности
+  document.getElementById('feedbackBlock').innerHTML = '<p>Спасибо за отзыв!</p>';
+}
