c29da07e7f9647605f49ec2faa668e37bdf7b04e
l1me <102348214+l1me0ffical@users.noreply.github.com>
2023-12-06 19:11:51 +0500

    Added new animations

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function submitFeedback() {
     //Заменить блок обратной связи на сообщение благодарности
     document.getElementById('feedback-card').innerHTML = '<p>Спасибо за отзыв!</p>';
 }
+
+
+let prevScrollPos = window.pageYOffset;
+const menu = document.querySelector('.navigation-buttons');
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // Листаем вверх
+    menu.classList.remove('hidden');
+  } else {
+    // Листаем вниз
+    menu.classList.add('hidden');
+  }
+
+  prevScrollPos = currentScrollPos;
+};
